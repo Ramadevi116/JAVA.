f1ebdd58e4c5aae9e1883fcfdf1ea157b8884cb0
hi <pokalaramadevi116@gmail.com>
2021-09-16 22:59:23 +0530

    Packages

---
+++ Java packages/packages.java
@@ -0,0 +1,20 @@
+package pak;
+public class pro {
+public void display() {
+		System.out.println("Hello All");
+        System.out.println("Welcome to java");
+	}
+}
+
+import pak.*;
+public class pro2 {
+public static void main(String[] args) {
+pro a=new pro();
+a.display();
+	}
+}
+
+// o/p:
+// Hello All
+// Welcome to java
+
